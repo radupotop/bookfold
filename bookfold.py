f4b24ed09bd51d1cb5d932e867017a78e3ef305b
kanak@localhost.localdomain <kanak@localhost.localdomain>
2010-08-20 00:48:48 -0400

    Refactored into methods the logic to (1) create an output writer that duplicated content, (2) create an output writer with rearranged content
    Also wrote a helper to generate output names based on input file name.

--- bookfold.py
+++ bookfold.py
@@ -8,7 +8,58 @@ Algorithm described at:
 http://blogs.gnome.org/cneumair/2007/03/15/printing-instruction-manuals-how-to-reorder-postscript-files/
 """
 import sys
+import math
+from os.path import splitext
 from pyPdf import PdfFileWriter, PdfFileReader
+
+def generate_output_name(input_name):
+    """
+    mybook.pdf -> mybook-bookfold.pdf
+    hello.pdf -> hello-bookfold.pdf
+    """
+    (root, ext) = splitext(input_name)
+    suffix = "-bookfold"
+    return root + suffix + ext
+
+def get_cloned_output_writer(inp, blank):
+    """
+    Returns a PdfFileWriter object containing pages from input padded with pages from blank so that total number of pages is a multiple of 4.
+    """
+    output = PdfFileWriter()
+    num_pages = inp.getNumPages()
+    
+    # Copy Input Pages
+    for page in xrange(num_pages):
+        output.addPage(inp.getPage(page))
+
+    # Pad if necessary
+    if num_pages % 4 == 0:
+        return output
+
+    blank_to_add = int(math.ceil(num_pages / 4.0)) * 4 - num_pages
+
+    for _ in xrange(blank_to_add):
+        output.addPage(blank.getPage(0))
+    
+    return output
+
+def get_rearranged_output_writer(inp):
+    """
+    Returns a PdfFileWriter object which rearranges the output from input so that it is suitable for a booklet style printing.
+    """
+    output = PdfFileWriter()
+    total_pages = inp.getNumPages()
+
+    for i in range(1, total_pages/2 + 1):
+        if i % 2:
+            output.addPage(inp.getPage(total_pages - i))
+            output.addPage(inp.getPage(i - 1))
+            print total_pages + 1 - i, "," ,i, "," ,
+        else:
+            output.addPage(inp.getPage(i - 1))
+            output.addPage(inp.getPage(total_pages - i))
+            print i , "," , total_pages + 1 - i, "," ,
+    return output
     
 def main():
     # Handle Arguments
@@ -17,49 +68,26 @@ def main():
         sys.exit(1)
     else:
         print sys.argv[1]
-        inputDoc = sys.argv[1]
+        input_doc = sys.argv[1]
 
     blank  = PdfFileReader(file("blank.pdf", "rb"))
-    input1 = PdfFileReader(file(inputDoc, "rb"))
-    output = PdfFileWriter()
+    input1 = PdfFileReader(file(input_doc, "rb"))
 
-    # copy input doc to output
-    numPages = input1.getNumPages()
-    for page in range(numPages):
-        output.addPage(input1.getPage(page))
-
-    # append blank pages until total no. of pages is a multiple of 4
-    blankPages = 0
-    while ((numPages+blankPages) % 4):
-        blankPages += 1
-    for page in range(blankPages):
-        output.addPage(blank.getPage(0))
+    # Copy the Input Document to Output (padding if necessary)
+    output = get_cloned_output_writer(input1, blank)
 
     # write temp
-    outputStream = file("temp.pdf", "wb")
-    output.write(outputStream)
-    outputStream.close()
+    with file("temp.pdf", "wb") as outputStream:
+        output.write(outputStream)
 
     # read temp as input
     input2 = PdfFileReader(file("temp.pdf", "rb"))
-    output = PdfFileWriter()
-    totalPages = input2.getNumPages()
-    
-    # re-arrange pages
-    for i in range(1, totalPages/2+1):
-        if i % 2:
-            output.addPage(input2.getPage(totalPages-i))
-            output.addPage(input2.getPage(i-1))
-            print totalPages+1-i,",",i,",",
-        else:
-            output.addPage(input2.getPage(i-1))
-            output.addPage(input2.getPage(totalPages-i))
-            print i,",",totalPages+1-i,",",
+    output = get_rearranged_output_writer(input2)
 
     # write output
-    outputStream = file("output.pdf", "wb")
-    output.write(outputStream)
-    outputStream.close()
+    output_doc = generate_output_name(input_doc)
+    with file(output_doc, "wb") as outputStream:
+        output.write(outputStream)
 
     import os
     os.remove("temp.pdf")
