2a202aba6a65480f7f49fbf5e3e8c684ce7ba30a
kanak@localhost.localdomain <kanak@localhost.localdomain>
2010-08-20 00:12:14 -0400

    Removed class and replaced with a simple main function.
    Fixed Indentation to be 4 spaces.

--- bookfold.py
+++ bookfold.py
@@ -1,61 +1,68 @@
 #!/usr/bin/python
-#
-# Usage: ./bookfold.py input.pdf
+"""
+BookFold: Makes a PDF suitable for printing
 
-class bookfold():
-	'''
-	Transforms input PDF into a PDF that's printable as a book.
-	Algorithm described here <http://blogs.gnome.org/cneumair/2007/03/15/printing-instruction-manuals-how-to-reorder-postscript-files/>
-	'''
+Usage: ./bookfold.py input.pdf
 
-	def __init__(self, inputDoc):
-		from pyPdf import PdfFileWriter, PdfFileReader
-
-		blank  = PdfFileReader(file("blank.pdf", "rb"))
-		input1 = PdfFileReader(file(inputDoc, "rb"))
-		output = PdfFileWriter()
-
-		# copy input doc to output
-		numPages = input1.getNumPages()
-		for page in range(numPages):
-			output.addPage(input1.getPage(page))
-
-		# append blank pages until total no. of pages is a multiple of 4
-		blankPages = 0
-		while ((numPages+blankPages) % 4):
-			blankPages += 1
-		for page in range(blankPages):
-			output.addPage(blank.getPage(0))
-
-		# write temp
-		outputStream = file("temp.pdf", "wb")
-		output.write(outputStream)
-		outputStream.close()
-
-		# read temp as input
-		input2 = PdfFileReader(file("temp.pdf", "rb"))
-		output = PdfFileWriter()
-		totalPages = input2.getNumPages()
-
-		# re-arrange pages
-		for i in range(1, totalPages/2+1):
-			if i % 2:
-				output.addPage(input2.getPage(totalPages-i))
-				output.addPage(input2.getPage(i-1))
-				print totalPages+1-i,",",i,",",
-			else:
-				output.addPage(input2.getPage(i-1))
-				output.addPage(input2.getPage(totalPages-i))
-				print i,",",totalPages+1-i,",",
-
-		# write output
-		outputStream = file("output.pdf", "wb")
-		output.write(outputStream)
-		outputStream.close()
-
-		import os
-		os.remove("temp.pdf")
-
-
+Algorithm described at:
+http://blogs.gnome.org/cneumair/2007/03/15/printing-instruction-manuals-how-to-reorder-postscript-files/
+"""
 import sys
-bookfold = bookfold(sys.argv[1])
+from pyPdf import PdfFileWriter, PdfFileReader
+    
+def main():
+    # Handle Arguments
+    if len(sys.argv) < 2:
+        print "Usage: bookfold.py input.pdf"
+        sys.exit(1)
+    else:
+        print sys.argv[1]
+        inputDoc = sys.argv[1]
+
+    blank  = PdfFileReader(file("blank.pdf", "rb"))
+    input1 = PdfFileReader(file(inputDoc, "rb"))
+    output = PdfFileWriter()
+
+    # copy input doc to output
+    numPages = input1.getNumPages()
+    for page in range(numPages):
+        output.addPage(input1.getPage(page))
+
+    # append blank pages until total no. of pages is a multiple of 4
+    blankPages = 0
+    while ((numPages+blankPages) % 4):
+        blankPages += 1
+    for page in range(blankPages):
+        output.addPage(blank.getPage(0))
+
+    # write temp
+    outputStream = file("temp.pdf", "wb")
+    output.write(outputStream)
+    outputStream.close()
+
+    # read temp as input
+    input2 = PdfFileReader(file("temp.pdf", "rb"))
+    output = PdfFileWriter()
+    totalPages = input2.getNumPages()
+    
+    # re-arrange pages
+    for i in range(1, totalPages/2+1):
+        if i % 2:
+            output.addPage(input2.getPage(totalPages-i))
+            output.addPage(input2.getPage(i-1))
+            print totalPages+1-i,",",i,",",
+        else:
+            output.addPage(input2.getPage(i-1))
+            output.addPage(input2.getPage(totalPages-i))
+            print i,",",totalPages+1-i,",",
+
+    # write output
+    outputStream = file("output.pdf", "wb")
+    output.write(outputStream)
+    outputStream.close()
+
+    import os
+    os.remove("temp.pdf")
+
+if __name__ == "__main__":
+    main()
